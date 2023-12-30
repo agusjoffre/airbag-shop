2c84c9977815ac946b26821445bfa7e332c3e868
agusjoffre <aguslj16@gmail.com>
2023-12-30 15:56:55 -0300

    feat: shop background

--- src/components/Shopping/Shop.jsx
+++ src/components/Shopping/Shop.jsx
@@ -1,8 +1,14 @@
 import React from "react"
 import "../../styles/shop.css"
 
+const backgroundSrc = "src/assets/backgrounds/special-rectangle-background.png"
+
+
 function Shop () {
-    return null
+    return (
+        <section className="shop-section">
+        </section>
+    )
 }
 
 export default Shop
