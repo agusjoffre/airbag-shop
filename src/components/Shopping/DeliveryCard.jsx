8e31098c5820863383d3de8be9567cb452094f63
agusjoffre <aguslj16@gmail.com>
2023-12-30 16:25:55 -0300

    feat: start shop styling and components

---
+++ src/components/Shopping/DeliveryCard.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+export default function DeliveryCard ( { deliveryProps } ) {
+    return (
+        <div className="card-delivery">
+            <div className="card-delivery-left">
+                <img src={ deliveryProps.icon } width={ 15 } height={ 15 } alt={ `${ deliveryProps.title }. ${ deliveryProps.description }` } />
+            </div>
+            <div className="card-delivery-right">
+                <h4>{deliveryProps.title}</h4>
+                <p>{deliveryProps.description}</p>
+            </div>
+        </div>
+    )
+}
--- src/components/Shopping/Shop.jsx
+++ src/components/Shopping/Shop.jsx
@@ -1,12 +1,43 @@
 import React from "react"
+import DeliveryCard from "./DeliveryCard"
 import "../../styles/shop.css"
 
-const backgroundSrc = "src/assets/backgrounds/special-rectangle-background.png"
+const deliveryProps = [
+    {
+        id: 1,
+        icon: "src/assets/icons/truck.svg",
+        title: "Enviamos tu compra",
+        description: "Entregas a todo el pais"
+    },
+    {
+        id: 2,
+        icon: "src/assets/icons/credit-card.svg",
+        title: "Paga como quieras",
+        description: "Tarjetas de credito o efectivo"
+    },
+    {
+        id: 3,
+        icon: "src/assets/icons/lock.svg",
+        title: "Compra con seguridad",
+        description: "Tus datos siempre protegidos"
+    }
+]
+
+
 
 
 function Shop () {
     return (
         <section className="shop-section">
+            <div className="shop-top">
+                <h1 className="shop-title">TIENDA</h1>
+                <div className="delivery-info">
+                    <DeliveryCard deliveryProps={deliveryProps[0]}/>
+                    <DeliveryCard deliveryProps={deliveryProps[1]}/>
+                    <DeliveryCard deliveryProps={deliveryProps[2]}/>
+                </div>
+            </div>
+            <div className="shop-bottom"></div>
         </section>
     )
 }
